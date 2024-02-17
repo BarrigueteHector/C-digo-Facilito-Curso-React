2d6e08a13ef7a45dcf3919d6208c351eb7995951
BarrigueteHector <hectorrodjls@gmail.com>
2024-02-16 18:26:13 -0600

    Modulo 2 - Clase 4

--- src/MyFirstComponent.jsx
+++ src/MyFirstComponent.jsx
@@ -1,8 +1,20 @@
+//Permite actualizar un valor cada que sea necesario
+import {useState} from 'react';
+
 //Primera forma de declarar un componente (función flecha o anónima)
 const MyFirstComponent = () => {
+  const [value, setValue] = useState(0 /*Argumento*/ ); //value = valor/ setValue = set para actualizar / Argumento = valor inicial
   const hola = 'Hola';
 
-  return <div> {hola}, este es mi primer componente </div>;
+  //Hasta este momento no es reactivo, para actualizarlo se puede usar un temporizador
+
+  setTimeout(() => {
+    setValue(value + 1);
+  }, 3000); //3 segundos
+
+  //Ahora es reactivo
+
+  return <div> {hola}, este es mi primer componente {value}</div>;
 };
 
 //Segunda forma de declarar un componente (función tradicional)
